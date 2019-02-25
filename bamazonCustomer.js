22ad9482daca86e6df528aa5df0537922f9190b5
Robert O <romohund@gmail.com>
2019-02-24 22:30:27 -0600

    Partially tested code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,37 +1,104 @@
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-var connection = mysql.createConnection ({
+var connection = mysql.createConnection({
     host: "localhost",
-    port: 8000,
+    port: 3306,
     user: "root",
     password: "trebor",
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-if (err) throw err; 
+connection.connect(function (err) {
+    if (err) throw err;
 
-products();
+    products();
 });
 
-function products () {
-    connection.query("SELECT * FROM PRODUCTS", function (err,res) {
+function products() {
+    connection.query("SELECT * FROM PRODUCTS", function (err, res) {
         if (err) throw err;
 
-        console.log (" ");
-        console.log ("-------------Inventory-----------");
-        console.log (" ");
+        console.log(" ");
+        console.log("-------------Inventory-----------");
+        console.log(" ");
 
         for (var i = 0; i < res.length; i++) {
-            console.log ("Item ID: " + res[i].id);
-            console.log ("Product: " + res[i].product);
-            console.log ("Department: " + res[i].department);
+            console.log("Item ID: " + res[i].item_id);
+            console.log("Product: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
             console.log("Price: " + res[i].price);
-            console.log("Quantity Left: " + res[i].quantity);
-            console.log (" ");
-            console.log (" ");
+            console.log("Quantity Left: " + res[i].stock_quantity);
+            console.log(" ");
+            console.log(" ");
 
         }
     });
 }
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw console.log("Error with connection: " + err);
+        inquirer.prompt([
+            {
+                name: "selectId",
+                type: "input",
+                message: "Enter the ID number you want to purchase: ",
+            },
+            {
+                name: "amountBought",
+                type: "input",
+                message: "How many would you like?",
+            }
+        ]).then(function (answers) {
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, {
+                item_id: answers.selectId
+            }, function (err, res) {
+                var inStock = res[0].stock_quantity;
+                var itemBought = answers.amountBought;
+
+                if (inStock >= itemBought) {
+                    var leftInStock = inStock - itemBought;
+                    var totalPrice = res[0].price * itemBought;
+                    var itemPurchased = res[0].product_name;
+
+                    console.log(totalPrice + " total price of items bought");
+                    connection.query("UPDATE products SET ? WHERE ?", [
+                        {
+                            stock_quantity: leftInStock
+                        },
+                        {
+                            item_id: answers.selectId
+                        }
+                    ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("============================");
+                            console.log("\n\r");
+                            console.log("Order Details: ");
+                            console.log("Item bought: " + itemPurchased);
+                            console.log("Quantity bought: " + itemBought + " for $" + res[0].price);
+                            console.log("\n\r");
+                            console.log("Thank you for shopping with us! Have a nice day!");
+                            console.log("==============================");
+                            products();
+                        }
+                    );
+                }
+                else {
+                    console.log("==============================================");
+                    console.log("\n\r");
+                    console.log("Sorry! There is not enough of that product");
+                    console.log("\n\r");
+                    console.log("==============================================");
+                    products();
+                }
+
+
+
+            });
+        });
+    });
+}
+
